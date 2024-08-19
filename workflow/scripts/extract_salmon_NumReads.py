f8867e6ad79944a49212fe43006f31a1ef1197ba
lingyi-owl <60360947+lingyi-owl@users.noreply.github.com>
2024-08-19 13:40:38 +0000

    Rename the "NumReads" column to the base name of the input file

--- workflow/scripts/extract_salmon_NumReads.py
+++ workflow/scripts/extract_salmon_NumReads.py
@@ -1,16 +1,26 @@
-import sys
 import pandas as pd
+import os
+import sys
 
 def extract_salmon_num_reads(infile, outfile):
+    # Get the base name of the input file without the extension
+    base_name = os.path.splitext(os.path.basename(infile))[0]
+
     # Read the input file into a DataFrame
     df = pd.read_csv(infile, sep='\t')
 
-    # Extract only the columns with headers "name" and rename "NumReads" to the input file name
+    # Extract the "Name" and "NumReads" columns
     selected_columns = df[['Name', 'NumReads']]
+
+    # Rename the "NumReads" column to the base name of the input file
+    selected_columns.rename(columns={'NumReads': base_name}, inplace=True)
 
     # Write the selected columns to a new file in the output folder
     selected_columns.to_csv(outfile, sep='\t', index=False)
 
+# Get the input and output file paths from the command-line arguments
 infile = sys.argv[1]
 outfile = sys.argv[2]
+
+# Call the function with the provided arguments
 extract_salmon_num_reads(infile, outfile)
