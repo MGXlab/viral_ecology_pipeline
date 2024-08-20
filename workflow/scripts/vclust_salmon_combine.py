2589d94f16ca0f585ef8a3b0b289e8c29d9b45cc
lingyi-owl <60360947+lingyi-owl@users.noreply.github.com>
2024-08-20 13:23:38 +0000

    debug

--- workflow/scripts/vclust_salmon_combine.py
+++ workflow/scripts/vclust_salmon_combine.py
@@ -4,7 +4,7 @@ import sys
 def vclust_salmon_genera_merge(vclust_file, salmon_normalized_file, vclust_drep_abundance_file):
     
     # load the vclust file
-    vclust_df = pd.read_csv(vclust_file)
+    vclust_df = pd.read_csv(vclust_file, sep = '\t')
     
     # load the salmon_normalized file
     salmon_normalized_df = pd.read_csv(salmon_normalized_file)
@@ -18,6 +18,8 @@ def vclust_salmon_genera_merge(vclust_file, salmon_normalized_file, vclust_drep_
     # group the merged_df by "genera_drep_id" and sum the values
     merged_df = merged_df.groupby("genera_drep_id").sum()
 
+    # save the merged_df to the output file
+    merged_df.to_csv(vclust_drep_abundance_file, sep=',')
 
 # Get the input and output file paths from the command-line arguments
 vclust_file = sys.argv[1]
