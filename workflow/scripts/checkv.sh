8a30590a13a97f3655aacdbc2ec4e57304bb74a5
lingyi-owl <60360947+lingyi-owl@users.noreply.github.com>
2024-08-23 13:11:59 +0000

    add threads to checkv script

--- workflow/scripts/checkv.sh
+++ workflow/scripts/checkv.sh
@@ -1,4 +1,5 @@
 checkv end_to_end \
     /net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/abundant_genera/abundance_gt0.0001_25samples_genera_representative_contig_seqs.fasta \
     /net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/checkv \
-    -d /net/phage/linuxhome/mgx/people/jose/lingyi/database/checkv-db-v1.5
+    -d /net/phage/linuxhome/mgx/people/jose/lingyi/database/checkv-db-v1.5 \
+    -t 78
