3d021474c70d601a946fc792c1ae31d3eecea6ab
lingyi-owl <60360947+lingyi-owl@users.noreply.github.com>
2024-08-26 12:01:27 +0000

    modified

--- workflow/scripts/phold_features_merge_with_salmon_NumReads.py
+++ workflow/scripts/phold_features_merge_with_salmon_NumReads.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-phold_products_f = '/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/phold_gbk_product/almond_phold_products.csv'
+phold_products_f = '/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/phold_gbk_product/almond_phold_products_functions.csv'
 phold_products_df = pd.read_csv(phold_products_f, sep=",")
 print('phold_products_df is loaded')
 abundance_f = '/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/vclust_salmon/vclust_genera_drep_abundance_gt0_file.csv'
@@ -11,15 +11,15 @@ print('abundance_df is loaded')
 merged_df = pd.merge(phold_products_df, abundance_df, left_on="Sequence", right_on="genera_drep_id", how="left")
 print('merged_df is created')
 
-# Drop the "genera_drep_id" and "Sequence" columns
-merged_df = merged_df.drop(columns=["genera_drep_id", "Sequence"])
-print('columns "genera_drep_id" and "Sequence" are dropped')
+# Drop the "genera_drep_id", "Sequence", and "Product" columns
+merged_df = merged_df.drop(columns=["genera_drep_id", "Sequence", "Product"])
+print('columns "genera_drep_id", "Sequence", and "Product" are dropped')
 
 # Group merged DataFrame by "Product" and sum the abundance for each product
-grouped_df = merged_df.groupby('Product').sum().reset_index()
+grouped_df = merged_df.groupby('Functions').sum().reset_index()
 print('grouped_df is created')
 
 # Save the merged DataFrame to a CSV file
-output_file = '/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/phold_gbk_product/almond_phold_products_abundance_across_samples.csv'
+output_file = '/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/phold_gbk_product/almond_phold_functions_abundance_across_samples.csv'
 grouped_df.to_csv(output_file, index=False)
 print(f"Output file is saved to {output_file}")
