d50b4c63f5cb941231103a2460857f08b58fbba6
lingyi-owl <60360947+lingyi-owl@users.noreply.github.com>
2024-08-28 13:39:53 +0000

    modified

--- workflow/scripts/test_script.py
+++ workflow/scripts/test_script.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 # Specify the path to your GenBank file
 genbank_file = "/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/megapharokka_output/seqs0/pharokka.gbk"
+print(f"Processing {genbank_file}")
 
 # Lists to store the extracted LOCUS and VERSION values
 data = []
-
+print("create a emtpy list to store the extracted LOCUS and VERSION values")
 # Parse the GenBank file
 for record in SeqIO.parse(genbank_file, "genbank"):
     # Extract LOCUS (which is the ID of the sequence record)
@@ -19,10 +20,12 @@ for record in SeqIO.parse(genbank_file, "genbank"):
 
     # Append the values to the respective lists
     data.append([locus, version])
-
+    
     # # Print the extracted LOCUS and VERSION
     # print(f"LOCUS: {locus}, VERSION: {version}")
 # Create a DataFrame from the data
 df = pd.DataFrame(data, columns=["LOCUS", "VERSION"])
-# Optionally, you can store the extracted values in a data structure or write them to a file
-
+print("create a DataFrame from the data")
+# Write the DataFrame to a CSV file
+df.to_csv("/net/phage/linuxhome/mgx/people/jose/lingyi/almond_snakemake/combined_results/magapharokka_gbk_locus_version/seqs0_pharokka_locus_version.csv", index=False)
+print("write the DataFrame to a CSV file")
